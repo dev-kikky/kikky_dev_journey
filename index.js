e92eb1ae7b1d6b7685039bf92fd44d20d7c47621
Jonathan Peace Kikelomo <peacekikelomo35@gmail.com>
2026-01-31 19:41:06 +0100

    start learning html

---
+++ index.js
@@ -0,0 +1,18 @@
+// variables and data 
+// data is anything that reps a value eg name, class, profession, bio, age
+// variables are containers for storing data 
+// const variableName: doesn't change
+const pi=3.142
+
+// var variableName: can be changed
+var radius = 1.4
+
+// let variableName: is used as a local variable
+let name ="Peace"
+
+pi =34
+// console.log(item) is used to print item on the console() or terminal
+console.log(pi)
+
+// comments 
+// comment
